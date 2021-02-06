0516764a35f4177c24f7f7eb4b95286dbbee229b
susanwulfhawes <62613493+susanwulfhawes@users.noreply.github.com>
2021-02-06 17:47:51 -0500

    Susan Branch

--- src/components/JobsList.js
+++ src/components/JobsList.js
@@ -3,7 +3,7 @@ import React from 'react';
 const JobsList = (props) => {
     return ( 
         <>
-        <p>Hello from JobsList!</p>
+        <p>Hello from JobsList! and Susan</p>
         </>
      );
 }
