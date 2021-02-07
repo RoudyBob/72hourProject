e2bf2247391cee644f86aa1f59c83cbace9ad5b4
Abigail White <as.white@aol.com>
2021-02-07 17:35:41 -0500

    made it pretty and got the weather

--- src/components/OpenWeather.js
+++ src/components/OpenWeather.js
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Button } from 'reactstrap';
 
 const OpenWeather = (props) => {
+
+    const [weather, setWeather] = useState('');
+    const apiKey = "69d1b6b1d0ad2eb70808d612c592235b";
+
+
+    const fetcher = (e) => {
+        e.preventDefault();
+
+          fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${props.latitude}&lon=${props.longitude}&appid=${apiKey}&units=imperial`)
+           .then(res => res.json())
+           .then(json => {
+            console.log(json)
+            setWeather(json);
+           })
+         }
+
     return ( 
-        <>
-        <p>Hello from OpenWeather!</p>
-        </>
+        <div>
+            {weather.main ? <p>The weather in {weather.name} is <b>{weather.main.temp}° F</b> with {weather.weather[0].description}.</p> : <><br /><br /></>}
+            {weather.main ? <p><i>High:</i> <b>{weather.main.temp_max}° F</b></p> : <><br /><br /></>}
+            {weather.main ? <p><i>Low:</i> <b>{weather.main.temp_min}° F</b></p> : <><br /><br /></>}
+        <Button outline color="warning" onClick={fetcher}>Fahrenheit</Button>
+        </div>
      );
 }
  
